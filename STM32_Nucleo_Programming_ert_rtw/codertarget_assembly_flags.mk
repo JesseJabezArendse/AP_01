9da14a711cb95d44983ac18c934c9f23af9c56a5
JesseJabezArendse <ARNJES009@myuct.ac.za>
2024-08-25 19:29:33 +0200

    Added IKS02A1 and VL53L1A1

--- STM32_Nucleo_Programming_ert_rtw/codertarget_assembly_flags.mk
+++
@@ -1,5 +0,0 @@
-ASFLAGS_ADDITIONAL = -mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard
-TARGET_LOAD_CMD = 
-TARGET_LOAD_CMD_ARGS = 
-TARGET_PKG_INSTALLDIR = C:/PROGRA~3/MATLAB/SupportPackages/R2023a/toolbox/target/supportpackages/stmnucleo/registry/..
-STACK_SIZE = 64
