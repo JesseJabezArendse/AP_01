a07e79e2ecd87fc76d225b37df0f947a097ac6bb
JesseJabezArendse <ARNJES009@myuct.ac.za>
2025-01-06 15:58:24 +0200

    touch ups
    touch ups

--- src/iks02a1_error.m
+++ src/iks02a1_error.m
@@ -1,14 +1,13 @@
 classdef iks02a1_error < matlab.System
     methods (Access = protected)
-        function stepImpl(obj, u)
-            if u == true
+        function stepImpl(obj, trigger)
+            if trigger == true
                 % Display the custom message
                 disp("IKS02A1 Error - Unplug and Replug the Nucleo!");
                 
                 % Stop the simulation
-                error('Simulink:CustomError', 'Simulation stopped due to an IKS02A1 error.');
+                error('Simulink:NucleoError', 'Unplug and Replug the Nucleo!');
             end
-            y = u; % Pass the input to the output if no error occurs
         end
     end
 end
