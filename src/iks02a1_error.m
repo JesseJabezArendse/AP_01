3fc65f5cc585db52280bcd16a2ac646f86eaa4f6
JesseJabezArendse <ARNJES009@myuct.ac.za>
2025-01-08 00:45:06 +0200

    fixed scales

--- src/iks02a1_error.m
+++ src/iks02a1_error.m
@@ -2,11 +2,8 @@ classdef iks02a1_error < matlab.System
     methods (Access = protected)
         function stepImpl(obj, trigger)
             if trigger == true
-                % Display the custom message
-                disp("IKS02A1 Error - Unplug and Replug the Nucleo!");
-                
                 % Stop the simulation
-                error('Simulink:NucleoError', 'Unplug and Replug the Nucleo!');
+                error('NucleoError: Unplug and Replug the Nucleo!');
             end
         end
     end
